3319519d5c699b0463587c6d04dceb84e548e1a2
Jerold Albertson <jerold.albertson@gmail.com>
2013-12-08 12:48:43 -0700

    pso work

--- pso.py
+++ pso.py
@@ -77,6 +77,7 @@ class Particle:
 		Particle.particles.append(self)
 		if self.pIndex == 0:
 			Particle.best = self
+		self.bestInterMemberDistance = 9999.9
 
 	def resetMemberInfo(self):
 		self.members = []
@@ -85,13 +86,23 @@ class Particle:
 	def initVelocity(self, position):
 		v = []
 		for i, _ in enumerate(position):
-			v.append(float(random.randrange(-1000, 1000))/1000)
+			v.append(float(random.randrange(-1000, 1000))/3000)
 		return v
 
 	def updateFitness(self):
 		# minimize inter cluster distance
 		if len(self.memberDistances) > 0:
 			self.fitness = sum(self.memberDistances)/len(self.memberDistances)
+
+			# maxInterMemberDistance = 0
+			# for member1 in self.members:
+			# 	for member2 in self.members:
+			# 		dist = euclidianDistance(member1['p'], member2['p'])
+			# 		if dist > maxInterMemberDistance:
+			# 			maxInterMemberDistance = dist
+			# if maxInterMemberDistance < self.bestInterMemberDistance:
+			# 	self.bestInterMemberDistance = maxInterMemberDistance
+			# self.fitness = self.fitness + maxInterMemberDistance
 			
 			mean = patternsMean(self.members)
 			sd = patternsStandardDeviation(self.members, mean)
@@ -101,21 +112,22 @@ class Particle:
 			interClusterComponent = patternVarience(self.x, mean)
 			# print("Intra:" + str(intraClusterComponent))
 			# print("Inter:" + str(interClusterComponent))
+
+			if self.fitness < self.bFitness:
+				self.bx = euclidianCopy(self.x)
+				self.bFitness = self.fitness
+				if self.bFitness < Particle.best.bFitness:
+					Particle.best = self
 		else:
 			self.fitness = 99999.9
 
-		if self.fitness < self.bFitness:
-			self.bx = euclidianCopy(self.x)
-			self.bFitness = self.fitness
-			if self.bFitness < Particle.best.bFitness:
-				Particle.best = self
 
 	def updatePosition(self):
 		self.updateV()
 		self.updateX()
 
 	def updateV(self):
-		#nbx = self.neighborhoodBest()
+		nbx = self.neighborhoodBest()
 		gbx = self.globalBest()
 		# if self.pIndex == 0:
 		# 	print("VB: " + str(self.v))
@@ -124,7 +136,9 @@ class Particle:
 		# 	print("NX: " + str(nbx))
 		# 	print("GX: " + str(gbx))
 		for i, v in enumerate(self.v):
-			self.v[i] = v + .05*(self.bx[i] - self.x[i]) + .05*(gbx[i] - self.x[i])
+			self.v[i] = .98*v + .05*(self.bx[i] - self.x[i]) + 0.1*random.random()
+			# self.v[i] = v + .05*(self.bx[i] - self.x[i]) + .01*(nbx[i] - self.x[i]) + .01*(gbx[i] - self.x[i])
+			# self.v[i] = v + .05*(self.bx[i] - self.x[i]) + .05*(gbx[i] - self.x[i])
 
 	def updateX(self):
 		# Effectively turns PSO into K-Means
@@ -147,9 +161,9 @@ class Particle:
 		neighborCount = 2
 		best = Particle.particles[0]
 		for particle in Particle.particles[self.pIndex:(self.pIndex+neighborCount)%len(Particle.particles)]:
-			if particle.fitness < best:
+			if particle.bFitness < best:
 				best = particle
-		return best.x
+		return best.bx
 
 	def globalBest(self):
 		return Particle.best.bx
@@ -162,20 +176,50 @@ class Particle:
 
 
 class Swarm:
+	minV = []
+	maxV = []
+
 	def __init__(self, particleCount, patterns):
 		self.patterns = []
 		for p in patterns:
 			self.patterns.append(p)
-		self.particles = []
-		patSample = random.sample(self.patterns, particleCount)
-		for p in patSample:
-			newPart = Particle(p['p'])
-			self.particles.append(newPart)
+		self.particles = self.initParticles(particleCount, patterns)
+		# patSample = random.sample(self.patterns, particleCount)
+		# for p in patSample:
+		# 	newPart = Particle(p['p'])
+		# 	self.particles.append(newPart)
 			# print(newPart)
 
+	def initParticles(self, particleCount, patterns):
+		Swarm.minV = euclidianCopy(patterns[0]['p'])
+		Swarm.maxV = euclidianCopy(patterns[0]['p'])
+		for p in patterns:
+			for i, x in enumerate(p['p']):
+				if x < Swarm.minV[i]:
+					Swarm.minV[i] = x
+				if x > Swarm.maxV[i]:
+					Swarm.maxV[i] = x
+		parts = []
+		for _ in range(particleCount):
+			parts.append(Particle(self.randomPositionInRange()))
+		return parts
+
+	def randomPositionInRange(self):
+		newPattern = emptyCopy(self.patterns[0]['p'])
+		for i, _ in enumerate(newPattern):
+			newPattern[i] = float(random.randrange(int(Swarm.minV[i]*1000), int(Swarm.maxV[i]*1000)))/1000
+		return newPattern
+
 	def updateMembers(self):
+		self.checkProximity()
 		self.updateMembership()
 		self.updateParticles()
+
+	def checkProximity(self):
+		for p1 in self.particles:
+			for p2 in self.particles:
+				if p1.pIndex != p2.pIndex and euclidianDistance(p1.bx, p2.bx) < 1.0:
+					p1.initVelocity(self.randomPositionInRange())
 
 	def updateMembership(self):
 		for particle in self.particles:
@@ -197,21 +241,8 @@ class Swarm:
 	def updateParticles(self):
 		for particle in self.particles:
 			particle.updateFitness()
-			if False:
-				print(	str(particle.pIndex) + 
-						", Members: " + str(len(particle.members)) +
-						", Distance: " + str(sum(particle.memberDistances)) +
-						", Fitness: " + str(particle.fitness) + 
-						", X: " + ", ".join(str(v) for v in particle.x))
 		for particle in self.particles:
 			particle.updatePosition()
-			if False:
-				print(	
-						str(particle.pIndex) + 
-						", Members: " + str(len(particle.members)) +
-						", Distance: " + str(sum(particle.memberDistances)) +
-						", Fitness: " + str(particle.fitness) + 
-						", X: " + ", ".join(str(v) for v in particle.x))
 
 if __name__=="__main__":
 	# Batch: (ordered by least time complex to most)
@@ -246,14 +277,14 @@ if __name__=="__main__":
 		pSet = PatternSet(dataSet)
 		for pat in pSet.patterns:
 			pat['h'] = []
-		swarm = Swarm(4, pSet.patterns)
+		swarm = Swarm(10, pSet.patterns)
 		startTime = time.time()
 
 
 		sinceBestChange = 0
 		previousBest = 9999.9
 		# while sinceBestChange < 1000:
-		for _ in range(1000):
+		for i in range(1000):
 			swarm.updateMembers()
 			bestDelta = previousBest - sum(p.bFitness for p in Particle.particles)
 			previousBest = sum(p.bFitness for p in Particle.particles)
